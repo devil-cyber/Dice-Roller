3c03dbeed951e30ceb6fbae3faf0019d050132ec
Manikant Singh <mani2474695@gmail.com>
2020-10-05 10:08:13 +0530

    imported homePage.dart to main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,18 @@
+import "package:flutter/material.dart";
+import "homePage.dart";
 
+//TODO:Update pubspec file for image assets first
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Dice Roller',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(primarySwatch: Colors.yellow),
+      home: HomePage(),
+    );
+  }
+}
